f45efed29300a08c3c96b07dabccbdbd1dafcd32
Christopher Maldonado <chrisis14@tamu.edu>
2025-03-30 20:56:02 -0500

    farming commits

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -5,7 +5,7 @@ import invisible from './Nothing.jpg'
 const Home = () => {
   return (
     <div>
-      <h1>Nothing is happening here</h1>
+      <h1>Nothing is happening here, pretty neat right?</h1>
       <img  className = "Hohn" src = {invisible} alt = "picture of the best group"></img><br/>
       <Link to="/Malogin">
           <button className="manButton">Go to Manager Login Page</button>
