fca67493a4d4ea1a441e265b33d9e47deb539be3
Jianwei Gao <jianwei_gao@tamu.edu>
2025-03-31 21:37:08 -0500

    deleting and editing item for backend

--- server/index.js
+++ server/index.js
@@ -4,6 +4,9 @@ const pool = require('./db');
 require("dotenv").config();
 const drinksRoutes = require("./routes/drinks");
 const sendOrderRoutes = require("./routes/send_order");
+const deleteItem = require("./routes/delete_item");
+const editItem = require("./routes/edit_item");
+
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -14,6 +17,9 @@ app.use(express.json());
 
 app.use("/api/drinks", drinksRoutes);
 app.use("/api/send_orders", sendOrderRoutes);
+app.use("/api/delete", deleteItem);
+app.use("/api/edit", editItem);
+
 
 // Test route
 app.get("/", (req, res) => {
--- server/routes/send_order.js
+++ server/routes/send_order.js
@@ -4,21 +4,21 @@ const pool = require("../db");
 const router = express.Router();
 
 /*
-request body takes an array of (employee_id, drink_id, ice_level, sugar_level, [add_ons])
+request takes an array of (employee_id, drink_id, ice_level, sugar_level, [add_ons]) and creates a new order in table
 for example an order with three drinks have req.body:
-  {orders: [
-    [1, 15, 2, 3, ['black_pearl', 'mini_pearl']],
-    [1, 2, 1, 2, []],
-    [1, 3, 4, 4, ['pudding']]
+    [
+        [1, 15, 2, 3, ['black_pearl', 'mini_pearl']],
+        [1, 2, 1, 2, []],
+        [1, 3, 4, 4, ['pudding']]
     ]
   }
 */
 router.post("/", async (req, res) => {
-    console.log("Begin sending order");
+    //console.log("Begin sending order");
     try {
         const orders = req.body;
         if(!Array.isArray(orders) || orders.length === 0){
-          return res.status(400).send('Invalid or empty queries array');
+          return res.status(400).json({error:'Invalid or empty queries array'});
         }
         const order_number = (await pool.query("SELECT max(order_number)+1 AS num FROM orders;")).rows[0].num;
         const timenow = (new Date()).toISOString().replace("T"," ").substring(0, 19);
@@ -43,13 +43,18 @@ router.post("/", async (req, res) => {
           });
           let sql = `INSERT INTO orders (order_number, employee_id, drink_id, ice_level, sugar_level, add_ons, order_date, price) 
           VALUES (${order_number}, ${employee_id}, ${drink_id}, ${ice_level}, ${sugar_level}, '${add_on_text}', '${timenow}', ${price});`
-          //console.log(sql);
-          const result = await pool.query(sql);
+          try{
+            const result = await pool.query(sql);
+          }
+          catch(err){
+            res.status(400).json({message: "Query Error", error: err.message});
+          }
         }
-        console.log("Order logged");
+        //console.log("Order logged");
+        res.status(200).json({message: "Order sent"});
     } catch (err) {
-        console.error(err.message);
-        res.status(500).json({ error: "Server error" });
+        //console.error(err.message);
+        res.status(500).json({ message:"Server error", error: err.message });
     }
 });
 
