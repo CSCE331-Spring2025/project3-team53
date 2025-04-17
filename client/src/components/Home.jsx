6b8650978384dee6bcb10a0761bd860087593e1f
Christopher Maldonado <chrisis14@tamu.edu>
2025-04-16 21:13:33 -0500

    I swear this change was necessary.

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 
 function Home() {
     return (
-        <div>
+        <center>
             <header>
                 <h1>Bruhba Bobruh Tea</h1>
             </header>
@@ -12,18 +12,16 @@ function Home() {
                 <p>Bruhba Bobruh is dedicated to surpassing customer expectations through carefully crafted and reliable solutions. Established by Alex Pierce, Aaron Mai, Jianwei Gao, and Chris Maldonado, our company is driven by one goal: putting customers at the center of everything we do. With a mission to deliver products that meet your needs at exceptional value and quality, Bruhba Bobruh ensures every order is tailored to your satisfaction. Why wait? Experience Bruhba Bobruh today!</p>
                 <p></p>
                 <nav>
-                    <ul>
                         <li><Link to="/Customer">Customer Login</Link></li>
                         <li><Link to="/Emplogin" >Employee Login</Link></li>
                         <li><Link to="/Malogin">Manager Login</Link></li>
                         <li><Link to="/Debug">Debug Page</Link></li>
-                    </ul>
                 </nav>
             </main>
             <footer>
                 <p>© 2025 Bruhba. All rights reserved.</p>
             </footer>
-        </div>
+        </center>
     );
 }
 
