c1850f97e55908c9dc5398e531b9ae761b687f45
Jianwei Gao <jianwei739321@gmail.com>
2025-04-26 21:31:16 -0500

    x-report front and back-end integration

--- client/src/components/Logging.jsx
+++ client/src/components/Logging.jsx
@@ -1,7 +1,33 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { BarChart } from "@mui/x-charts/BarChart"
+import * as func from "../apiCall"
 
 const Logging = () => {
+  const series = [
+    {dataKey: "sum_dollars", label: "dollar earned"},
+    {dataKey: "sum_drinks", label: "indivdual drinks ordered"},
+    {dataKey: "sum_orders", label: "total orders"},
+  ]
+
+  const [data, setData] = useState([]);
+  const [date, setDate] = useState("");
+  const [hourF, sethourF] = useState("11");
+  const [hourT, sethourT] = useState("22");
+
+  const handleMakeReport = async () => {
+    let res = (await func.order_hist(date, hourF, hourT));
+    if(res.hasOwnProperty('data')){
+      let data = res.data
+      let newHrs = [], newData = [];
+      data.forEach((row) => {
+        newHrs.push(row.hr)
+        newData.push({hrs: row.hr, sum_dollars: Number(row.sum_dollar), sum_drinks:Number(row.sum_drinks), sum_orders:Number(row.sum_orders)});
+      })
+      setData(newData);
+    }
+  }
+
   return (
     <>
       <h2 className="login-header">X-Report</h2>
@@ -18,14 +44,37 @@ const Logging = () => {
           justifyContent: "center", 
         }}
       >
-        <div style={{ textAlign: "center" }}>
-          <p>This is placeholder to make sure it can store the X Report.</p>
-        </div>
+        <center style={{width: "80vw", height: "30vw"}}>
+          <BarChart
+            xAxis={[{ scaleType: 'band', dataKey: "hrs", label: "Hour" }]}
+            series={series}
+            dataset = {data}
+            height={500}
+          />
+        </center>
       </div>
-      <input className = "date"type="date" placeholder="Select a date" />
+      <input className = "date" type="date" placeholder="Select a date" onChange={(e) => {setDate(e.target.value)}}/>
       <br/>
+      
+      <label for="from">From: </label>
+      <select name="from" id="from" value={hourF} onChange={(e) => {sethourF(e.target.value)}}>
+        {Array.from({ length: 12 }, (_, i) => {
+          const hour = i + 11;
+          return <option key={hour} value={hour}>{hour}:00</option>;
+        })}
+      </select>
+
+      <label for="to">To: </label>
+      <select name="to" id="to" value={hourT} onChange={(e) => {sethourT(e.target.value)}}>
+        {Array.from({ length: 12 }, (_, i) => {
+          const hour = i + 11;
+          return <option key={hour} value={hour}>{hour}:00</option>;
+        })}
+      </select>
+
+
       <div>
-        <button className="log">Make Report</button>
+        <button className="log" onClick={() => {handleMakeReport()}}>Make Report</button>
         <button className="log">Clear</button>
       </div>
       <Link to="/Manager">
